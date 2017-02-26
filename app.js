cc3e719b3dba66c84ce1853de92fe19794a710b3
Michael Tsao <caoxiang@yeah.net>
2017-02-26 13:51:09 +0800

    -

--- app.js
+++ app.js
@@ -1,73 +1,73 @@
 //app.js
 App({
-  onLaunch: function () {
-    //调用API从本地缓存中获取数据
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-  },
+    onLaunch: function () {
+        //调用API从本地缓存中获取数据
+        var logs = wx.getStorageSync('logs') || []
+        logs.unshift(Date.now())
+        wx.setStorageSync('logs', logs)
+    },
 
-  getUserInfo:function(cb){
-    var that = this
-    if(this.globalData.userInfo){
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
-      //调用登录接口
-      wx.login({
-        success: function () {
-          wx.getUserInfo({
+    getUserInfo: function (cb) {
+        var that = this
+        if (this.globalData.userInfo) {
+            typeof cb == "function" && cb(this.globalData.userInfo)
+        } else {
+            //调用登录接口
+            wx.login({
+                success: function () {
+                    wx.getUserInfo({
+                        success: function (res) {
+                            that.globalData.userInfo = res.userInfo
+                            typeof cb == "function" && cb(that.globalData.userInfo)
+                        }
+                    })
+                }
+            })
+        }
+    },
+
+    checkToken: function (success, fail) {
+        var token = wx.getStorageSync('token')
+
+        if (token) {
+            this.requestApi('user/login', {"token": token}, 'get', success, fail)
+        } else {
+            fail()
+        }
+    },
+
+    requestApi: function (url, param, method, successFunc, failFunc) {
+        wx.request({
+            url: this.globalData.host + url,
+            data: param,
+            method: method,
+            header: {
+                'content-type': 'application/x-www-form-urlencoded'
+            },
             success: function (res) {
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
+                console.log("api result")
+                console.log(res)
+                if (typeof successFunc == "function") {
+                    successFunc(res)
+                }
+            },
+            fail: function (res) {
+                wx.showToast({
+                    title: '网络异常',
+                    icon: 'loading',
+                    duration: 1000
+                })
+
+                if (typeof failFunc == "function") {
+                    failFunc(res)
+                }
             }
-          })
-        }
-      })
-    }
-  },
-
-  checkToken:function(success, fail){
-    var token = wx.getStorageSync('token')
-
-    if(token){
-      this.requestApi('user/login', {"token": token}, 'get', success, fail)
-    }else{
-      fail()
-    }
-  },
-
-  requestApi:function(url, param=null, method='get', successFunc=null, failFunc=null){
-    wx.request({
-      url: this.globalData.host+url,
-      data: param,
-      method: method,
-      header: {
-          'content-type': 'application/x-www-form-urlencoded'
-      },
-      success: function(res) {
-        console.log("api result")
-        console.log(res)
-        if (typeof successFunc == "function"){
-          successFunc(res)
-        }
-      },
-      fail: function(res) {
-        wx.showToast({
-          title: '网络异常',
-          icon: 'loading',
-          duration: 1000
         })
+    },
 
-        if(typeof failFunc == "function"){
-          failFunc(res)
-        }
-      }
-    })
-  },
-
-  globalData:{
-    uid: null,
-    host:"http://er.cx/",
-    hospitals:[]
-  }
+    globalData: {
+        uid: null,
+        host: "http://er.cx/",
+        hospitals: []
+    }
 })
--- pages/chooseRange/choose.js
+++ pages/chooseRange/choose.js
@@ -2,52 +2,54 @@
 var items = []
 var ids = []
 var pageObject = {
-  data: {
-    rangeItems: [],
-    rangeId: [],
-    rangeSelect: null
-  },
-  selectRange: function(e) {
-    this.setData({
-      rangeItems: e
-    })
-  },
-  onLoad: function () {
-      var app = getApp();
-      var that = this
-      
-      wx.request({
-        url: app.globalData.host + 'range/list',
-        header: {
-            'content-type': 'application/json'
-        },
-        success: function(res) {
-          var ids = []
-          var names = []
-          for (var id in res.data){
+    data: {
+        rangeItems: [],
+        rangeId: [],
+        rangeSelect: null
+    },
+
+    selectRange: function (e) {
+        this.setData({
+            rangeItems: e
+        })
+    },
+
+    onLoad: function () {
+        var app = getApp();
+        var that = this
+
+        app.requestApi('range/list',
+            {},
+            'get',
+            that.getOk,
+            null
+        )
+    },
+
+    getOk: function (res) {
+        var ids = []
+        var names = []
+        for (var id in res.data) {
             ids.push(id)
             names.push(res.data[id])
-          }
-          console.log(names)
-          items = names
-          that.setData({
+        }
+        items = names
+        this.setData({
             rangeItems: names,
             rangeId: ids
-          })
-        }
-      })
-  }
+        })
+    }
 }
 
 for (var i = 0; i < items.length; ++i) {
-  (function(itemIndex) {
-    pageObject['bind' + itemIndex] = function(e) {
-      console.log('click' + ids[itemIndex], e)
-      wx.navigateTo({
-        url: '../test/test?id='+ids[itemIndex]
-      })
-    }
-  })(i)
+    (function (itemIndex) {
+        pageObject['bind' + itemIndex] = function (e) {
+            console.log('click' + ids[itemIndex], e)
+            wx.navigateTo({
+                url: '../test/test?id=' + ids[itemIndex]
+            })
+        }
+    })(i)
 }
 
 Page(pageObject)
